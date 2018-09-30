b077eb592da2923ea376cd38942d64cb67e35f13
Daniel de Souza Martins <daniel.martins@way2.com.br>
2018-09-30 19:59:49 -0300

    Criação da estrutura base da API
    - Criação da estrutura base da API;
    - Adição de dois serviços mock como base;
    - Adição da referência ao Swagger e das devidas configurações;
    - Criação da classe FormateComentariosXml para formatar corretamente caracteres especiais como & < > na documentação dos serviços para exibição no Swagger.

---
+++ CalculadorDeJurosApi/Controllers/CalculadorDeJurosController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CalculadorDeJurosApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculadorDeJurosController : ControllerBase
+    {
+        /// <summary>
+        /// Calcula o valor final após aplicar a taxa de juros ao longo do tempo.
+        /// </summary>
+        /// <remarks>
+        /// Exemplo de requisição:
+        ///     POST /CalculadorDeJuros/calculajuros?<paramref name="valorInicial"/>=100&amp;<paramref name="meses"/>=5
+        /// </remarks>
+        /// <param name="valorInicial">Valor base ao qual será aplicado o juros ao longo do tempo.</param>
+        /// <param name="meses">Tempo em mêses para o cálculo do juros.</param>
+        /// <returns>Valor final calculado.</returns>  
+        /// <response code="200">Retorna quando o valor é calculado com sucesso.</response>         
+        [HttpPost]
+        [ProducesResponseType(200)]
+        public ActionResult<decimal> CalculaJuros([FromQuery] decimal valorInicial, [FromQuery] int meses)
+        {
+            return 200;
+        }
+
+        /// <summary>
+        /// Retorna o endereço do projeto no GitHub.
+        /// </summary>
+        /// <remarks>
+        /// Exemplo de requisição:
+        ///     GET /CalculadorDeJuros/showmethecode
+        /// </remarks>
+        /// <returns>Endereço do projeto no GitHub.</returns>  
+        /// <response code="200">Retorna quando o endereço é retornado com sucesso.</response>    
+        [HttpGet]
+        public ActionResult<string> ShowMeTheCode() => "em breve";
+    }
+}
---
+++ CalculadorDeJurosApi/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
+using CalculadorDeJurosApi.Helpers;
+
+namespace CalculadorDeJurosApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "CalculadorDeJurosApi",
+                    Description = "Uma Web API ASP.NET Core com documentação em Swagger.",
+                    Contact = new Contact
+                    {
+                        Name = "Daniel de Souza Martins",
+                        Email = string.Empty,
+                        Url = "https://www.linkedin.com/in/daniel-de-souza-martins/"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under MIT",
+                        Url = "https://opensource.org/licenses/MIT"
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.OperationFilter<FormateComentariosXml>();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CalculadorDeJurosApi");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseMvc();
+        }
+    }
+}
